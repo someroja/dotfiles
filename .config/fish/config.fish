a152d778887351390e06190eeb4275bf040c7d27
Osmo Someroja <osmo.someroja@kodan.fi>
2022-06-16 15:55:07 +0300

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,12 @@ if status is-interactive && not functions --query fisher
     curl -sL https://git.io/fisher | source && fisher update
 end
 
+# Configure direnv
+if type --query direnv
+    direnv hook fish | source
+    set -g direnv_fish_mode eval_on_arrow
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g async_prompt_functions _pure_prompt_git
