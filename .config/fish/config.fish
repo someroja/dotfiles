45ceb4dc1e81a51aac0b86fb1bc0c18ec9a42a9b
Osmo Someroja <osmo@someroja.com>
2022-06-17 00:58:35 +0300

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+if status is-login
+    set -gx LANG en_US.UTF-8
+    set -gx LC_ALL fi_FI.UTF-8
+end
+
 # Ensure that we have Fisher
 if status is-interactive && not functions --query fisher
     curl -sL https://git.io/fisher | source && fisher update
@@ -9,12 +14,6 @@ if type --query direnv
     set -g direnv_fish_mode eval_on_arrow
 end
 
-if status is-login
-    set -gx LANG en_US.UTF-8
-    set -gx LC_ALL fi_FI.UTF-8
-end
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     set -g async_prompt_functions _pure_prompt_git
 end
