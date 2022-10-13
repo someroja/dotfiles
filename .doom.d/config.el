60b1b44fd7da45c9e7308216fbd15c04e52316ab
Osmo Someroja <osmo@someroja.com>
2022-10-13 20:34:51 +0300

    Update Doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,25 +3,10 @@
 (setq user-full-name "Osmo Someroja"
       user-mail-address "osmo@someroja.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 18)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 18)
+      doom-big-font (font-spec :family "IBM Plex Mono" :size 36)
+      doom-serif-font (font-spec :family "IBM Plex Serif" :size 18))
 
 ; Load local config if we have it
 (load! "config.local" doom-user-dir t)
