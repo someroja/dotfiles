795cefba81677260534b17f400d6cae526fb8382
Osmo Someroja <osmo.someroja@kodan.fi>
2023-01-30 10:02:21 +0200

    Update neovim settings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Copy to system clipboard
+vim.keymap.set({ "n", "v" }, "<Leader>y", [["+y]])
+vim.keymap.set("n", "<Leader>yy", [["+yy]])
+vim.keymap.set("n", "<Leader>Y", [["+Y]])
+
+-- Paste from system clipboard
+vim.keymap.set({ "n", "v" }, "<Leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<Leader>P", [["+P]])
