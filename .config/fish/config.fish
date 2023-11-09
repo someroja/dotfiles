e3e21b135121b919ba657feed7452f85039bb583
Osmo Someroja <osmo@someroja.com>
2023-11-09 22:49:35 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,16 @@
 if type --query kak
     set -gx EDITOR kak
+    abbr --add k kak
 else
     set -gx EDITOR vim
 end
 
 if type --query fnm
     fnm env --use-on-cd | source
+end
+
+if type -- query lazygit
+    abbr --add lg lazygit
 end
 
 abbr --add df dotfiles
