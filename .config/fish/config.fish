06f018250cb22b4af6b8374b2b1dc9de801bb733
Osmo Someroja <osmo@someroja.com>
2024-06-11 22:29:06 +0300

    Add support for MacPorts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
-if type --query kak
-    set -gx EDITOR kak
-    abbr --add k kak
-else
-    set -gx EDITOR vim
+if test -e /opt/local/bin/port
+    fish_add_path /opt/local/bin /opt/local/sbin
 end
+
+set -gx EDITOR vim
 
 if type --query fnm
     fnm env --use-on-cd | source
