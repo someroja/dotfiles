991c128c0d782e6a6034c712cadabdddb1dd5cb3
Osmo Someroja <osmo@someroja.com>
2024-09-21 22:42:10 +0300

    Update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,9 +1,9 @@
 set -gx EDITOR vim
 
-if type --query fnm
-    fnm env --use-on-cd | source
-end
-
 if type --query lazygit
     abbr --add lg lazygit
+end
+
+if type --query fnm
+    fnm env --use-on-cd | source
 end
--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -1,3 +1,10 @@
-function fish_prompt --description 'Write out the prompt'
-    echo -nse '\n' (set_color normal) (prompt_pwd --dir-length=0) (set_color brpurple) (fish_vcs_prompt) (set_color normal) '\n> '
+function fish_prompt --description "Write out the prompt"
+    set_color normal
+    echo -n (prompt_pwd --dir-length=0)
+
+    set_color purple
+    echo -n (fish_vcs_prompt)
+
+    set_color normal
+    echo -n "> "
 end
