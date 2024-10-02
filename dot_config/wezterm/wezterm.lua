aea655a290739cd9152bf05e033bcdaadc262d96
Osmo Someroja <osmo@someroja.com>
2024-10-02 22:30:56 +0300

    Update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require "wezterm"
 
 local config = wezterm.config_builder()
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 -- config.color_scheme = "nordfox"
 config.font_size = 16.0
 
