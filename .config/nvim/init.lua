822602cd477a3a1da80c64b3eecd6f438e87514e
Osmo Someroja <osmo@someroja.com>
2025-06-10 22:37:28 +0300

    Update Neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,2 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.scrolloff = 8
-
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+require("options")
+require("plugins")
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
